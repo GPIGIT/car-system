ababd9bdb65e5663c8ba6a3fbc3b8478080f0cb9
GitSpeedy <speedygonzales@abv.bg>
2023-08-23 20:13:20 +0300

    test run Spring with @RestController & @GetMapping

--- src/main/java/com/inventorycarsystem/carsystem/CarSystemApplication.java
+++ src/main/java/com/inventorycarsystem/carsystem/CarSystemApplication.java
@@ -3,16 +3,23 @@ package com.inventorycarsystem.carsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class CarSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarSystemApplication.class, args);
 	}
 
+	@GetMapping("/")
+		public String sayHello(){
+
+		return new String("HellO!");
+		}
 
 }
